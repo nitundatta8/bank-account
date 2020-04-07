05c79f0f4c39ccf2b599bac463cbf0afb9b9030e
nitunwa <nitun.2@gmail.com>
2020-04-07 16:53:50 -0700

    add function for show the total balance

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,12 @@ NewAccount.prototype.addMoney = function(deposit) {
 NewAccount.prototype.takeMoney = function(withdrawal) {
    account.balance = withdrawal;
 }
- 
+
+function showTotalBalance(account) {
+   var totalBalance = "<h2 class='showBalance'>Name: " + account.name + "<br>Total Balance: " + account.balance +"</h2";
+   $("#result").html(totalBalance);
+ }
+ var account ;
  
  $(document).ready(function(){
    $("form#account").submit(function(event) {
@@ -18,18 +23,27 @@ NewAccount.prototype.takeMoney = function(withdrawal) {
       var name = $("input#name").val();
       var balance = $("input#initialDeposit").val();
       console.log(name, balance);
-      $("input#name").val("");
-      $("input#initialDeposit").val("");
+      $("input#name").val('');
+      $("input#initialDeposit").val('');
+      account = new NewAccount(name, balance);
 
-      //var newAccount = new NewAccount(inputtedName,inputtedInitialDeposit);
+      //var newAccount() = new NewAccount(inputtedName,inputtedInitialDeposit);
       
       // newAccount.addMoney(inputtedDeposit)
-      return account = new NewAccount(name, balance);
+      //return account = new NewAccount(name, balance);
    });
-   //  $(".money").submit(function(event) {
-   //    event.preventDefault();
-   //    var inputtedDeposit = $("input#deposit").val();
-   //    var inputtedWithdrawal = $("input#withdrawal").val();
+    $(".money").submit(function(event) {
+      event.preventDefault();
+      var inputtedDeposit = $("input#deposit").val();
+      var inputtedWithdrawal = $("input#withdrawal").val();
+      if(inputtedDeposit !== '' && inputtedWithdrawal!=='' ){
+         alert("You are not allow to do both transaction togater.")
+      }
+      $("input#deposit").val('');
+      $("input#withdrawal").val('');
+      account.addMoney(inputtedDeposit);
+      account.takeMoney(inputtedWithdrawal);
+      showTotalBalance(account);
        
-   //  });
+    });
  });
